702b1fda5cc2030b5a68f940655cdc3a6a257ad8
ChengYan <zqalyc@163.com>
2022-10-19 15:34:31 +0800

    Update Oauth2ApplicationTests.java

--- balawo-oauth2/src/test/java/com/balawo/oauth2/Oauth2ApplicationTests.java
+++ balawo-oauth2/src/test/java/com/balawo/oauth2/Oauth2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Oauth2ApplicationTests {
+public class Oauth2ApplicationTests {
 
     @Test
     void contextLoads() {
