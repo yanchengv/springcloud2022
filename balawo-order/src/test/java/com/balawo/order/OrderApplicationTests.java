239ad21ca45264d54b784de3efa1a022c4bb1752
ChengYan <zqalyc@163.com>
2022-10-19 15:34:54 +0800

    Update OrderApplicationTests.java

--- balawo-order/src/test/java/com/balawo/order/OrderApplicationTests.java
+++ balawo-order/src/test/java/com/balawo/order/OrderApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderApplicationTests {
+public class OrderApplicationTests {
 
     @Test
     void contextLoads() {
